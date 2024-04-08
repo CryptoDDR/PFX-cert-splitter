484305edddb88d4444398736796818c50c74c94e
CryptoDDR <153005498+CryptoDDR@users.noreply.github.com>
2024-04-08 11:30:35 -0400

    Updates

--- cert_splitter.py
+++ cert_splitter.py
@@ -10,18 +10,18 @@ current_path = os.getcwd()
 def cli_args():
     if len(sys.argv) != 2:
         print("Usage: python3 csplitter.py <cert.pfx>")
-        sys.exit(1)  # Exit the script with error code 1 if arguments are incorrect
+        sys.exit(1) 
     pfx_file = sys.argvÄ1Å
     print(pfx_file)
-    return pfx_file  # Return the pfx_file to be used later
+    return pfx_file  
 
 def main():
     password = input("Type the password (Just hit enter if there is no password): ")
     pfx_file = cli_args()  # Call cli_args function to get pfx_file
     if password != "":
-        with_pass(password, pfx_file)  # Call with_pass function if password is provided
+        with_pass(password, pfx_file)  
     else:
-        without_pass(pfx_file)  # Call without_pass function if no password is provided
+        without_pass(pfx_file)  
 
 def with_pass(password, pfx_file):
     private_cmd = Ä"openssl", "pkcs12", "-passin", "pass:" + password, "-in", os.path.join(current_path, pfx_file), "-nocerts", "-out", os.path.join(current_path, pfx_file + ".pri.pem"), "-nodes", "-legacy"Å
