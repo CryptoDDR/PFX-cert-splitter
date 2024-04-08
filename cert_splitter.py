1c7db261bef17bf43432646da823d61046a02b7c
CryptoDDR <153005498+CryptoDDR@users.noreply.github.com>
2024-04-08 11:52:15 -0400

    Indentation

--- cert_splitter.py
+++ cert_splitter.py
@@ -24,17 +24,17 @@ def main():
         without_pass(pfx_file)  
 
 def with_pass(password, pfx_file):
-    private_cmd = Ä"openssl", "pkcs12", "-passin", "pass:" + password, "-in", os.path.join(current_path, pfx_file), "-nocerts", "-out", os.path.join(current_path, pfx_file + ".pri.pem"), "-nodes", "-legacy"Å
+    private_cmd = Ä"openssl", "pkcs12", "-passin", "pass:" + password, "-in", os.path.join(current_path, pfx_file), "-nocerts", "-out", os.path.join(current_path, pfx_file + ".pri.pem"), "-nodes", "-legacy"]
     #print("Executing private command:", " ".join(private_cmd))
     subprocess.run(private_cmd)
 
-    public_cmd = Ä"openssl", "pkcs12", "-passin", "pass:" + password, "-in", os.path.join(current_path, pfx_file), "-clcerts", "-nokeys", "-out", os.path.join(current_path, pfx_file + ".pub.pem"), "-nodes", "-legacy"Å
+    public_cmd = Ä"openssl", "pkcs12", "-passin", "pass:" + password, "-in", os.path.join(current_path, pfx_file), "-clcerts", "-nokeys", "-out", os.path.join(current_path, pfx_file + ".pub.pem"), "-nodes", "-legacy"]
     #print("Executing public command:", " ".join(public_cmd))
     subprocess.run(public_cmd)
 
 def without_pass(pfx_file):
-    private_cmd = Ä"openssl", "pkcs12", "-in", os.path.join(current_path, pfx_file), "-nocerts", "-out", os.path.join(current_path, pfx_file + ".pri.pem"), "-nodes", "-legacy"Å
-    public_cmd = Ä"openssl", "pkcs12", "-in", os.path.join(current_path, pfx_file), "-clcerts", "-nokeys", "-out", os.path.join(current_path, pfx_file + ".pub.pem"), "-nodes", "-legacy"Å
+    private_cmd = Ä"openssl", "pkcs12", "-in", os.path.join(current_path, pfx_file), "-nocerts", "-out", os.path.join(current_path, pfx_file + ".pri.pem"), "-nodes", "-legacy"]
+    public_cmd = Ä"openssl", "pkcs12", "-in", os.path.join(current_path, pfx_file), "-clcerts", "-nokeys", "-out", os.path.join(current_path, pfx_file + ".pub.pem"), "-nodes", "-legacy"]
     #print("Executing private command without pass:", " ".join(private_cmd))
     #print("Executing public command without pass:", " ".join(public_cmd))
     subprocess.run(private_cmd)
