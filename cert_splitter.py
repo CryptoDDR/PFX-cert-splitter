22f04f1a10e32892f2518a5843ed539434030d2b
CryptoDDR <153005498+CryptoDDR@users.noreply.github.com>
2024-07-24 12:22:17 -0400

    Added .cert feature

--- cert_splitter.py
+++ cert_splitter.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 import sys
+#print(DDR_Encoded)
 
 current_path = os.getcwd()
 #print(current_path)
@@ -26,18 +27,24 @@ def main():
 def with_pass(password, pfx_file):
     private_cmd = ["openssl", "pkcs12", "-passin", "pass:" + password, "-in", os.path.join(current_path, pfx_file), "-nocerts", "-out", os.path.join(current_path, pfx_file + ".pri.pem"), "-nodes", "-legacy"]
     #print("Executing private command:", " ".join(private_cmd))
-    subprocess.run(" ".join(private_cmd), shell=True)  
+    subprocess.run(" ".join(private_cmd), shell=True)
 
     public_cmd = ["openssl", "pkcs12", "-passin", "pass:" + password, "-in", os.path.join(current_path, pfx_file), "-clcerts", "-nokeys", "-out", os.path.join(current_path, pfx_file + ".pub.pem"), "-nodes", "-legacy"]
     #print("Executing public command:", " ".join(public_cmd))
-    subprocess.run(" ".join(public_cmd), shell=True) 
+    subprocess.run(" ".join(public_cmd), shell=True)
+    
+    cert_cmd = ["openssl", "pkcs12", "-passin", "pass:" + password, "-in", os.path.join(current_path, pfx_file), "-nokeys", "-out", os.path.join(current_path, pfx_file + ".crt"), "-nodes", "-legacy"]
+    subprocess.run(" ".join(cert_cmd), shell=True)
+
 
 def without_pass(pfx_file):
     private_cmd = ["openssl", "pkcs12", "-in", os.path.join(current_path, pfx_file), "-nocerts", "-out", os.path.join(current_path, pfx_file + ".pri.pem"), "-nodes", "-legacy"]
     public_cmd = ["openssl", "pkcs12", "-in", os.path.join(current_path, pfx_file), "-clcerts", "-nokeys", "-out", os.path.join(current_path, pfx_file + ".pub.pem"), "-nodes", "-legacy"]
+    cert_cmd = ["openssl", "pkcs12", "-in", os.path.join(current_path, pfx_file), "-nokeys", "-out", os.path.join(current_path, pfx_file + ".crt"), "-nodes", "-legacy"]
     #print("Executing private command without pass:", " ".join(private_cmd))
     #print("Executing public command without pass:", " ".join(public_cmd))
     subprocess.run(" ".join(private_cmd), shell=True)
     subprocess.run(" ".join(public_cmd), shell=True)
+    subprocess.run(" ".join(cert_cmd), shell=True)
 
 main()
